37617195488c926fdd989a01750442abbe733081
TommyYOyoyo <79941840+TommyYOyoyo@users.noreply.github.com>
2022-02-01 22:43:40 -0500

    A SPAM BOT MADE WITH PYTHON
    Use at your risks.

---
+++ spambot.py
@@ -0,0 +1,35 @@
+"""AUTOSPAM FOR FUN ONLY"""
+
+from subprocess import CompletedProcess
+import pyautogui
+import time
+time.sleep(0.5)
+
+def Register_AutoSpamInfo():
+    while True:
+        spamfile = open("/Users/lizhu/Tommy/Programming/Python/Other/spamtext.txt", "w")
+        print("Welcome to Autospammer, for fun only. Use at your risks.")
+        time.sleep(0.5)
+        print("Made with Python_")
+        time.sleep(1)
+        Spam_Msg = str(input("What message would you like to spam?  Message : "))
+        Spam_Times = int(input("How many times would you spam this message? Count : "))
+        for i in range(Spam_Times):
+            spamfile.write(Spam_Msg)
+            spamfile.write("\n")
+        print("SPAMMING STARTING IN 5 SECONDS! PLEASE PREPARE WELL!")
+        time.sleep(5)
+        break
+    
+ 
+Register_AutoSpamInfo()
+   
+spamfileRead = open("/Users/lizhu/Tommy/Programming/Python/Other/spamtext.txt", "r")
+for msg in spamfileRead:
+    pyautogui.typewrite(msg)
+    pyautogui.press("enter")
+print("SPAM COMPLETED")
+pyautogui.typewrite("AUTOSPAM COMPLETED")
+pyautogui.press("enter")
+    
+#TEST:testtesttest
